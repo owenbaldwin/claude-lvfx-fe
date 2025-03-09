e285f51a5f7d2e556dc4f3a13fa3b73030cf5010
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-03-09 20:22:27 +0000

    Add Loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent {
+  @Input() message: string = 'Loading...';
+  @Input() size: number = 50;
+}
