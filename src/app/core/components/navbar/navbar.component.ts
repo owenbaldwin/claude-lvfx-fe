81901527dcae6fb682f584d1a3f3aef8677ec121
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-03-09 20:08:40 +0000

    Add navbar component

---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '@app/shared/models';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  currentUser: User | null = null;
+  isAuthenticated = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
