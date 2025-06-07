c76668ae21512aa0e77a62bac5da006f4cf39243
Owen Baldwin <owenabaldwin@gmail.com>
2025-06-07 23:53:31 +0100

    shot elements in sidebar working

--- src/app/features/productions/sidebar/sidebar/sidebar.component.ts
+++ src/app/features/productions/sidebar/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComplexitiesComponent } from '../complexities/complexities.component';
+import { SidebarElementsComponent } from '../sidebar-elements/sidebar-elements.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, ComplexitiesComponent],
+  imports: [CommonModule, ComplexitiesComponent, SidebarElementsComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
